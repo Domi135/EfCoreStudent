694f24b944a856f98a06b3dcf142c3e872529718
domin <domin@DOMI>
2024-11-06 10:18:58 +0100

    Add project files.

---
+++ EfCoreStudent/Menu.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCoreStudent
+{
+    internal class Menu
+    {
+        public DbContext dbContext = new DbContextStudent();
+        
+        public void RegisterNewStudent()
+        {
+            Console.WriteLine("Skriv in info");
+            Console.Write("Namn: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Efternamn: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Stad: ");
+            string city = Console.ReadLine();
+            var student = new Student()
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                City = city
+            };
+            dbContext.Add(student);
+            dbContext.SaveChanges();
+        }
+
+        public void ChangeStudent()
+        {
+            var dbContext = new DbContextStudent();
+            Console.WriteLine("Skriv namnet på studenten du vill ändra på");        //här kan jag göra valet lite djupare o försöka välja en specifik student ist
+            string nameOfStudent = Console.ReadLine();
+            var changedStudent = dbContext.Students.Where(s => s.FirstName == nameOfStudent).FirstOrDefault<Student>();
+            Console.Write("Nytt namn: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Nytt efternamn: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Ny stad: ");
+            string city = Console.ReadLine();
+            changedStudent.FirstName = firstName;
+            changedStudent.LastName = lastName;
+            changedStudent.City = city;
+            dbContext.SaveChanges();
+        }
+
+        public void ListAllStudents()
+        {
+            var dbContext = new DbContextStudent();
+            foreach (var item in dbContext.Students)
+            {
+                Console.WriteLine($"Student id: {item.StudentId}, {item.FirstName} {item.LastName} {item.City}");
+            }
+        }
+    }
+}
---
+++ EfCoreStudent/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Xml.Serialization;
+
+namespace EfCoreStudent
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var Menu = new Menu();
+            var dbContext = new DbContextStudent();
+            while (true)
+            {
+                Console.WriteLine("Välj vad du vill göra (1-3)");
+                Console.WriteLine("1. Registrera ny student");
+                Console.WriteLine("2. Ändra student");
+                Console.WriteLine("3. Lista alla studenter");
+                Console.WriteLine("4. Avsluta");
+                int val = int.Parse(Console.ReadLine());
+                switch (val)
+                {
+                    case 1:
+                        Menu.RegisterNewStudent();
+                        break;
+
+                    case 2:
+                        Menu.ChangeStudent();
+                        break;
+
+                    case 3:
+                        Menu.ListAllStudents();
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Tack o hej! :)");
+                        return;
+
+                    default:
+                        Console.WriteLine("Någonting gick fel...");
+                        return;
+                }
+            }
+        }
+    }
+
+}
